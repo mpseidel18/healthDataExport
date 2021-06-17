4d97cf844b3f12978604b31aeae8700250eb1e18
Marius Seidel <mariuspseidel@gmail.com>
2021-06-17 23:40:47 +0200

    added click cli

--- getTcxData.py
+++ getTcxData.py
@@ -1,9 +1,4 @@
-from healthdataInterpret import printData
+from healthdataInterpret import *
 import sys
 import os
-d = str(sys.argv[1])
-print(sys.argv[1])
-for path in os.listdir(d):
-    full_path = os.path.join(d, path)
-    if os.path.isfile(full_path):
-        printData(full_path)
+
