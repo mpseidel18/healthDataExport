b137523cb4bcfabb7998c0326982f792335d6293
Marius Seidel <mariuspseidel@gmail.com>
2021-06-19 20:45:22 +0200

    finished first version of tool

--- automaticAnalyzer.py
+++ automaticAnalyzer.py
@@ -5,13 +5,5 @@ import csv
 from xlsxwriter.workbook import Workbook
 import os
 from glob import glob
-FILETYPES = ['tcx', 'json', 'xlsx','csv']
 
-def analyzeHealthData(PATH):
-    for extension in FILETYPES:
-        if extension == 'tcx':
-            getTCXDataToTxt(PATH, extension)
-        if extension == 'json':
-            convertJson2xlsx(PATH, extension)
-        if extension == 'csv':
-            convertCsv2Xlsx(PATH, extension)
+
